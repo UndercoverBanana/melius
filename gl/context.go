0becec6fd0f27714aa48be7b1bae9af8aa9ec6b5
InfinitiStudios <chris@infiniti.dev>
2021-01-02 14:05:10 -0600

    Update context.go

--- gl/context.go
+++ gl/context.go
@@ -1,8 +1,39 @@
 package gl
 
 import (
-	"github.com/go-gl/gl/v4.6-compatibility/gl"
+	"fmt"
+	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"strings"
+)
+
+const (
+	width  = 500
+	height = 500
+
+	vertexShaderSource = `
+		#version 410
+		in vec3 vp;
+		void main() {
+			gl_Position = vec4(vp, 1.0);
+		}
+	` + "\x00"
+
+	fragmentShaderSource = `
+		#version 410
+		out vec4 frag_colour;
+		void main() {
+			frag_colour = vec4(1, 1, 1, 1.0);
+		}
+	` + "\x00"
+)
+
+var (
+	triangle = []float32{
+		0, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+	}
 )
 
 // Initializes GLFW and sets up the Canvas window.
@@ -27,16 +58,63 @@ func CreateWindowCanvas(title string) {
 
 	win.Maximize()
 
+
 	for !win.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT)
 
-		gl.Begin(gl.TRIANGLES)
-		gl.Vertex2f(-0.5, -0.5)
-		gl.Vertex2f(0.0, 0.5)
-		gl.Vertex2f(0.5, -0.5)
-		gl.End()
+
 
 		win.SwapBuffers()
 		glfw.WaitEvents()
 	}
+}
+
+func draw(vao uint32, window *glfw.Window, program uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	gl.BindVertexArray(vao)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/3))
+
+	glfw.PollEvents()
+	window.SwapBuffers()
+}
+
+func makeVao(points []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vao
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
 }
